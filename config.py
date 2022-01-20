dcfae963dd88b9de0d5d3523cd4a32812885e29c
zane <chen7157783@gmail.com>
2022-01-20 19:47:03 +0800

    1

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 class Config(object):
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:0101czx724715@127.0.0.1:3306/epidemic"
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:0101czx7215@127.0.0.1:3306/epidemic"
-
+    SQLALCHEMY_CHARSET = 'utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True  # 会打印原生sql语句，便于观察测试
     SECRET_KEY="root"
